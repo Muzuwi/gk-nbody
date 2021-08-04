c2eb9fe38967e61f7a9b83de3092f5408278a148
Niiedzwiedz <vinter98@gmail.com>
2021-08-04 17:52:54 +0200

    Base Drawing v1.0

--- gk-nbody/Renderer.cs
+++ gk-nbody/Renderer.cs
@@ -8,7 +8,8 @@ namespace GKApp
     {
         private ISimulation _simulation;
         private ProgramHandle _program;
-        
+        private BufferHandle _vertexPositionBuffer;
+
         public Renderer(ISimulation simulation)
         {
             _simulation = simulation;
@@ -20,6 +21,7 @@ namespace GKApp
             
             string vertexShaderCode = @"
             #version 460 core
+            precision highp float;
             layout (location = 0) in vec3 a_pos;
 
             out vec3 v_color;
@@ -27,10 +29,12 @@ namespace GKApp
             {
                 v_color     = vec3(1.0, 0.0, 1.0);
                 gl_Position = vec4(a_pos, 0.0); 
+                gl_PointSize = 10.0;
             }";
 
             string fragmentShaderCode = @"
             #version 460 core
+            precision highp float;
             out vec3 frag_color;
             in  vec3 v_color;
       
@@ -48,6 +52,8 @@ namespace GKApp
             GL.GetShaderInfoLog(vertexHandle, out string vertexLog);
             if (vertexLog.Trim().Length != 0)
             {
+                System.Diagnostics.Debug.WriteLine("Failed compiling vertex shader");
+                System.Diagnostics.Debug.WriteLine(vertexLog);
                 Console.WriteLine("Failed compiling vertex shader");
                 Console.WriteLine(vertexLog);
                 return;
@@ -57,6 +63,8 @@ namespace GKApp
             GL.GetShaderInfoLog(fragmentHandle, out string fragmentLog);
             if (fragmentLog.Trim().Length != 0)
             {
+                System.Diagnostics.Debug.WriteLine("Failed compiling fragment shader");
+                System.Diagnostics.Debug.WriteLine(fragmentLog);
                 Console.WriteLine("Failed compiling fragment shader");
                 Console.WriteLine(fragmentLog);
                 return;
@@ -69,16 +77,26 @@ namespace GKApp
             GL.GetProgramInfoLog(_program, out string programLog);
             if (programLog.Trim().Length != 0)
             {
+                System.Diagnostics.Debug.WriteLine("Failed linking program");
+                System.Diagnostics.Debug.WriteLine(programLog);
                 Console.WriteLine("Failed linking program");
                 Console.WriteLine(programLog);
                 return;
             }
+
+            _vertexPositionBuffer = GL.CreateBuffer();
         }
         
         public void Render()
         {
             GL.UseProgram(_program);
-            
+
+            GL.BindBuffer(BufferTargetARB.ArrayBuffer, _vertexPositionBuffer);
+
+            double[] vertexPos = new double[3] { 0, 0, 0 };
+            GL.BufferData(BufferTargetARB.ArrayBuffer, vertexPos, BufferUsageARB.StaticDraw);
+            GL.DrawArrays(PrimitiveType.Points, 0, 1);
+
         }
     }
 }
